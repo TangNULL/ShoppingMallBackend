7bce57d340d73e66a3a3f6e28a5ad5428d3c19e7
Tangyf <1358336145@qq.com>
2022-05-02 17:27:26 +0800

    test conflict1
    test conflict1

--- springcloud-eureka-server/src/main/java/org/example/EurekaServerApplication.java
+++ springcloud-eureka-server/src/main/java/org/example/EurekaServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-//当前使用eureka的server
+//当前使用eureka的server.  ,,,,
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
